62fc47afffce56b82c8c453bb63071d90c87d9cd
Tim Henkes <me@syndace.dev>
2022-08-13 13:39:52 +0200

    Adjusted serialization code

--- oldmemo/etree.py
+++ oldmemo/etree.py
@@ -4,6 +4,7 @@ import xml.etree.ElementTree as ET
 
 from omemo import EncryptedKeyMaterial, KeyExchange, Message
 import x3dh
+import xeddsa
 try:
     import xmlschema
 except ImportError as e:
@@ -28,123 +29,115 @@ __all__ = [
 NS = f"{{{NAMESPACE}}}"
 
 
-DEVICE_LIST_SCHEMA = xmlschema.XMLSchema("""<?xml version='1.0' encoding='UTF-8'?>
-<xs:schema xmlns:xs='http://www.w3.org/2001/XMLSchema'
-           targetNamespace='eu.siacs.conversations.axolotl'
-           xmlns='eu.siacs.conversations.axolotl'>
+DEVICE_LIST_SCHEMA = xmlschema.XMLSchema("""<?xml version="1.0" encoding="utf8"?>
+<xs:schema xmlns:xs="http://www.w3.org/2001/XMLSchema"
+    targetNamespace="eu.siacs.conversations.axolotl"
+    xmlns="eu.siacs.conversations.axolotl">
 
-    <xs:element name='devices'>
-        <xs:complexType>
-            <xs:sequence minOccurs='0' maxOccurs='unbounded'>
-                <xs:element ref='device'/>
-            </xs:sequence>
-        </xs:complexType>
-    </xs:element>
+  <xs:element name="device">
+    <xs:complexType>
+      <xs:attribute name="id" type="xs:unsignedInt" use="required"/>
+    </xs:complexType>
+  </xs:element>
 
-    <xs:element name='device'>
-        <xs:complexType>
-            <xs:attribute name='id' type='xs:unsignedInt' use='required'/>
-            <xs:attribute name='label' type='xs:string'/>
-        </xs:complexType>
-    </xs:element>
+  <xs:element name="list">
+    <xs:complexType>
+      <xs:sequence minOccurs="0" maxOccurs="unbounded">
+        <xs:element ref="device"/>
+      </xs:sequence>
+    </xs:complexType>
+  </xs:element>
 </xs:schema>
 """)
 
 
-BUNDLE_SCHEMA = xmlschema.XMLSchema("""<?xml version='1.0' encoding='UTF-8'?>
-<xs:schema xmlns:xs='http://www.w3.org/2001/XMLSchema'
-           targetNamespace='eu.siacs.conversations.axolotl'
-           xmlns='eu.siacs.conversations.axolotl'>
+BUNDLE_SCHEMA = xmlschema.XMLSchema("""<?xml version="1.0" encoding="utf8"?>
+<xs:schema xmlns:xs="http://www.w3.org/2001/XMLSchema"
+    targetNamespace="eu.siacs.conversations.axolotl"
+    xmlns="eu.siacs.conversations.axolotl">
 
-    <xs:element name='bundle'>
-        <xs:complexType>
-            <xs:all>
-                <xs:element ref='spk'/>
-                <xs:element ref='spks'/>
-                <xs:element ref='ik'/>
-                <xs:element ref='prekeys'/>
-            </xs:all>
-        </xs:complexType>
-    </xs:element>
+  <xs:element name="signedPreKeyPublic">
+    <xs:complexType>
+      <xs:simpleContent>
+        <xs:extension base='xs:base64Binary'>
+          <xs:attribute name="signedPreKeyId" type="xs:unsignedInt"/>
+        </xs:extension>
+      </xs:simpleContent>
+    </xs:complexType>
+  </xs:element>
 
-    <xs:element name='spk'>
-        <xs:complexType>
-            <xs:simpleContent>
-                <xs:extension base='xs:base64Binary'>
-                    <xs:attribute name='id' type='xs:unsignedInt' use='required'/>
-                </xs:extension>
-            </xs:simpleContent>
-        </xs:complexType>
-    </xs:element>
+  <xs:element name='signedPreKeySignature' type='xs:base64Binary'/>
+  <xs:element name='identityKey' type='xs:base64Binary'/>
 
-    <xs:element name='spks' type='xs:base64Binary'/>
-    <xs:element name='ik' type='xs:base64Binary'/>
+  <xs:element name="preKeyPublic">
+    <xs:complexType>
+      <xs:simpleContent>
+        <xs:extension base='xs:base64Binary'>
+          <xs:attribute name="preKeyId" type="xs:unsignedInt" use="required"/>
+        </xs:extension>
+      </xs:simpleContent>
+    </xs:complexType>
+  </xs:element>
 
-    <xs:element name='prekeys'>
-        <xs:complexType>
-            <xs:sequence maxOccurs='unbounded'>
-                <xs:element ref='pk'/>
-            </xs:sequence>
-        </xs:complexType>
-    </xs:element>
+  <xs:element name="prekeys">
+    <xs:complexType>
+      <xs:sequence>
+        <xs:element ref="preKeyPublic" maxOccurs="unbounded"/>
+      </xs:sequence>
+    </xs:complexType>
+  </xs:element>
 
-    <xs:element name='pk'>
-        <xs:complexType>
-            <xs:simpleContent>
-                <xs:extension base='xs:base64Binary'>
-                    <xs:attribute name='id' type='xs:unsignedInt' use='required'/>
-                </xs:extension>
-            </xs:simpleContent>
-        </xs:complexType>
-    </xs:element>
+  <xs:element name="bundle">
+    <xs:complexType>
+      <xs:sequence>
+        <xs:element ref="signedPreKeyPublic"/>
+        <xs:element ref="signedPreKeySignature"/>
+        <xs:element ref="identityKey"/>
+        <xs:element ref="prekeys"/>
+      </xs:sequence>
+    </xs:complexType>
+  </xs:element>
 </xs:schema>
 """)
 
 
-MESSAGE_SCHEMA = xmlschema.XMLSchema("""<?xml version='1.0' encoding='UTF-8'?>
-<xs:schema xmlns:xs='http://www.w3.org/2001/XMLSchema'
-           targetNamespace='eu.siacs.conversations.axolotl'
-           xmlns='eu.siacs.conversations.axolotl'>
+MESSAGE_SCHEMA = xmlschema.XMLSchema("""<?xml version="1.0" encoding="utf8"?>
+<xs:schema xmlns:xs="http://www.w3.org/2001/XMLSchema"
+    targetNamespace="eu.siacs.conversations.axolotl"
+    xmlns="eu.siacs.conversations.axolotl">
 
-    <xs:element name='encrypted'>
-        <xs:complexType>
-            <xs:all>
-                <xs:element ref='header'/>
-                <xs:element ref='payload' minOccurs='0' maxOccurs='1'/>
-            </xs:all>
-        </xs:complexType>
-    </xs:element>
+  <xs:element name="payload" type="xs:base64Binary"/>
+  <xs:element name="iv" type="xs:base64Binary"/>
 
-    <xs:element name='payload' type='xs:base64Binary'/>
+  <xs:element name="key">
+    <xs:complexType>
+      <xs:simpleContent>
+        <xs:extension base='xs:base64Binary'>
+          <xs:attribute name="rid" type="xs:unsignedInt" use="required"/>
+          <xs:attribute name="prekey" type="xs:boolean"/>
+        </xs:extension>
+      </xs:simpleContent>
+    </xs:complexType>
+  </xs:element>
 
-    <xs:element name='header'>
-        <xs:complexType>
-            <xs:sequence maxOccurs='unbounded'>
-                <xs:element ref='keys'/>
-            </xs:sequence>
-            <xs:attribute name='sid' type='xs:unsignedInt'/>
-        </xs:complexType>
-    </xs:element>
+  <xs:element name="header">
+    <xs:complexType>
+      <xs:sequence>
+        <xs:element ref="key" maxOccurs="unbounded"/>
+        <xs:element ref="iv"/>
+      </xs:sequence>
+      <xs:attribute name="sid" type="xs:unsignedInt"/>
+    </xs:complexType>
+  </xs:element>
 
-    <xs:element name='keys'>
-        <xs:complexType>
-            <xs:sequence maxOccurs='unbounded'>
-                <xs:element ref='key'/>
-            </xs:sequence>
-            <xs:attribute name='jid' type='xs:string' use='required'/>
-        </xs:complexType>
-    </xs:element>
-
-    <xs:element name='key'>
-        <xs:complexType>
-            <xs:simpleContent>
-                <xs:extension base='xs:base64Binary'>
-                    <xs:attribute name='rid' type='xs:unsignedInt' use='required'/>
-                    <xs:attribute name='kex' type='xs:boolean' default='false'/>
-                </xs:extension>
-            </xs:simpleContent>
-        </xs:complexType>
-    </xs:element>
+  <xs:element name="encrypted">
+    <xs:complexType>
+      <xs:all>
+        <xs:element ref="header"/>
+        <xs:element ref="payload" minOccurs="0"/>
+      </xs:all>
+    </xs:complexType>
+  </xs:element>
 </xs:schema>
 """)
 
@@ -153,21 +146,20 @@ def serialize_device_list(device_list: Dict[int, Optional[str]]) -> ET.Element:
     """
     Args:
         device_list: The device list to serialize. The first entry of each tuple is the device id, and the
-            second entry is the optional label.
+            second entry is the optional label. Note that labels are not supported by this version of the
+            specification and will not be included in the XML.
 
     Returns:
         The serialized device list as an XML element.
     """
 
-    devices_elt = ET.Element(f"{NS}devices")
+    list_elt = ET.Element(f"{NS}list")
 
-    for device_id, label in device_list.items():
-        device_elt = ET.SubElement(devices_elt, f"{NS}device")
+    for device_id, _ in device_list.items():
+        device_elt = ET.SubElement(list_elt, f"{NS}device")
         device_elt.set("id", str(device_id))
-        if label is not None:
-            device_elt.set("label", label)
 
-    return devices_elt
+    return list_elt
 
 
 def parse_device_list(element: ET.Element) -> Dict[int, Optional[str]]:
@@ -177,7 +169,8 @@ def parse_device_list(element: ET.Element) -> Dict[int, Optional[str]]:
 
     Returns:
         The extracted device list. The first entry of each tuple is the device id, and the second entry is the
-        optional label.
+        optional label. Note that labels are not supported by this version of the specification, so all labels
+        will be set to ``None``.
 
     Raises:
         XMLSchemaValidationError: in case the element does not conform to the XML schema given in the
@@ -186,11 +179,7 @@ def parse_device_list(element: ET.Element) -> Dict[int, Optional[str]]:
 
     DEVICE_LIST_SCHEMA.validate(element)
 
-    return {
-        int(cast(str, device_elt.get("id"))): device_elt.get("label", None)
-        for device_elt
-        in element.iter(f"{NS}device")
-    }
+    return { int(cast(str, device_elt.get("id"))): None for device_elt in element.iter(f"{NS}device") }
 
 
 def serialize_bundle(bundle: BundleImpl) -> ET.Element:
@@ -204,28 +193,34 @@ def serialize_bundle(bundle: BundleImpl) -> ET.Element:
 
     bundle_elt = ET.Element(f"{NS}bundle")
 
+    identity_key_sign = (bundle.bundle.identity_key[31] >> 7) & 1
+    identity_key_serialized = b"\x05" + xeddsa.ed25519_pub_to_curve25519_pub(bundle.bundle.identity_key)
+    signed_pre_key_signature_mut = bytearray(bundle.bundle.signed_pre_key_sig)
+    signed_pre_key_signature_mut[63] |= identity_key_sign << 7
+    signed_pre_key_signature = bytes(signed_pre_key_signature_mut)
+
     ET.SubElement(
         bundle_elt,
-        f"{NS}spk",
-        attrib={ "id": str(bundle.signed_pre_key_id) }
+        f"{NS}signedPreKeyPublic",
+        attrib={ "signedPreKeyId": str(bundle.signed_pre_key_id) }
     ).text = base64.b64encode(bundle.bundle.signed_pre_key).decode("ASCII")
 
     ET.SubElement(
         bundle_elt,
-        f"{NS}spks"
-    ).text = base64.b64encode(bundle.bundle.signed_pre_key_sig).decode("ASCII")
+        f"{NS}signedPreKeySignature"
+    ).text = base64.b64encode(signed_pre_key_signature).decode("ASCII")
 
     ET.SubElement(
         bundle_elt,
-        f"{NS}ik"
-    ).text = base64.b64encode(bundle.bundle.identity_key).decode("ASCII")
+        f"{NS}identityKey"
+    ).text = base64.b64encode(identity_key_serialized).decode("ASCII")
 
     prekeys_elt = ET.SubElement(bundle_elt, f"{NS}prekeys")
     for pre_key in bundle.bundle.pre_keys:
         ET.SubElement(
             prekeys_elt,
-            f"{NS}pk",
-            attrib={ "id": str(bundle.pre_key_ids[pre_key]) }
+            f"{NS}preKeyPublic",
+            attrib={ "preKeyId": str(bundle.pre_key_ids[pre_key]) }
         ).text = base64.b64encode(pre_key).decode("ASCII")
 
     return bundle_elt
@@ -248,20 +243,42 @@ def parse_bundle(element: ET.Element, bare_jid: str, device_id: int) -> BundleIm
 
     BUNDLE_SCHEMA.validate(element)
 
-    spk_elt = cast(ET.Element, element.find(f"{NS}spk"))
-    pk_elts = list(element.iter(f"{NS}pk"))
+    spkp_elt = cast(ET.Element, element.find(f"{NS}signedPreKeyPublic"))
+    pkp_elts = list(element.iter(f"{NS}preKeyPublic"))
+
+    signed_pre_key_signature = base64.b64decode(cast(str, cast(ET.Element, element.find(
+        f"{NS}signedPreKeySignature"
+    )).text))
+
+    identity_key_sign = (signed_pre_key_signature[63] >> 7) & 1
+
+    signed_pre_key_signature_mut = bytearray(signed_pre_key_signature)
+    signed_pre_key_signature_mut[63] &= 0x7f
+    signed_pre_key_signature = bytes(signed_pre_key_signature_mut)
+
+    identity_key_serialized = base64.b64decode(cast(str, cast(ET.Element, element.find(
+        f"{NS}identityKey"
+    )).text))
+
+    assert identity_key_serialized[0] == 0x05
+
+    identity_key = xeddsa.curve25519_pub_to_ed25519_pub(identity_key_serialized[1:], bool(identity_key_sign))
 
     return BundleImpl(
         bare_jid,
         device_id,
         x3dh.Bundle(
-            base64.b64decode(cast(str, cast(ET.Element, element.find(f"{NS}ik")).text)),
-            base64.b64decode(cast(str, spk_elt.text)),
-            base64.b64decode(cast(str, cast(ET.Element, element.find(f"{NS}spks")).text)),
-            frozenset(base64.b64decode(cast(str, pk_elt.text)) for pk_elt in pk_elts)
+            identity_key,
+            base64.b64decode(cast(str, spkp_elt.text)),
+            signed_pre_key_signature,
+            frozenset(base64.b64decode(cast(str, pkp_elt.text)) for pkp_elt in pkp_elts)
         ),
-        int(cast(str, spk_elt.get("id"))),
-        { base64.b64decode(cast(str, pk_elt.text)): int(cast(str, pk_elt.get("id"))) for pk_elt in pk_elts }
+        int(cast(str, spkp_elt.get("signedPreKeyId"))),
+        {
+            base64.b64decode(cast(str, pkp_elt.text)): int(cast(str, pkp_elt.get("preKeyId")))
+            for pkp_elt
+            in pkp_elts
+        }
     )
 
 
@@ -280,30 +297,31 @@ def serialize_message(message: Message) -> ET.Element:
 
     header_elt = ET.SubElement(encrypted_elt, f"{NS}header", attrib={ "sid": str(message.device_id) })
 
-    for bare_jid in frozenset(encrypted_key_material.bare_jid for encrypted_key_material, _ in message.keys):
-        keys_elt = ET.SubElement(header_elt, f"{NS}keys", attrib={ "jid": bare_jid })
+    for encrypted_key_material, key_exchange in message.keys:
+        assert isinstance(encrypted_key_material, EncryptedKeyMaterialImpl)
 
-        keys = frozenset(key for key in message.keys if key[0].bare_jid == bare_jid)
-        for encrypted_key_material, key_exchange in keys:
-            assert isinstance(encrypted_key_material, EncryptedKeyMaterialImpl)
+        key_elt = ET.SubElement(
+            header_elt,
+            f"{NS}key",
+            attrib={ "rid": str(encrypted_key_material.device_id) }
+        )
 
-            key_elt = ET.SubElement(
-                keys_elt,
-                f"{NS}key",
-                attrib={ "rid": str(encrypted_key_material.device_id) }
-            )
+        authenticated_message = encrypted_key_material.serialize()
 
-            authenticated_message = encrypted_key_material.serialize()
+        if key_exchange is None:
+            key_elt.text = base64.b64encode(authenticated_message).decode("ASCII")
+        else:
+            assert isinstance(key_exchange, KeyExchangeImpl)
 
-            if key_exchange is None:
-                key_elt.text = base64.b64encode(authenticated_message).decode("ASCII")
-            else:
-                assert isinstance(key_exchange, KeyExchangeImpl)
-
-                key_elt.set("kex", "true")
-                key_elt.text = base64.b64encode(key_exchange.serialize(authenticated_message)).decode("ASCII")
+            key_elt.set("prekey", "true")
+            key_elt.text = base64.b64encode(key_exchange.serialize(authenticated_message)).decode("ASCII")
 
     if not message.content.empty:
+        ET.SubElement(
+            header_elt,
+            f"{NS}iv"
+        ).text = base64.b64encode(message.content.initialization_vector).decode("ASCII")
+
         ET.SubElement(
             encrypted_elt,
             f"{NS}payload"
@@ -312,11 +330,12 @@ def serialize_message(message: Message) -> ET.Element:
     return encrypted_elt
 
 
-def parse_message(element: ET.Element, bare_jid: str) -> Message:
+def parse_message(element: ET.Element, sender_bare_jid: str, recipient_bare_jid: str) -> Message:
     """
     Args:
         element: The XML element to parse the message from.
-        bare_jid: The bare JID of the sender.
+        sender_bare_jid: The bare JID of the sender.
+        recipient_bare_jid: The bare JID of the recipient, i.e. us.
 
     Returns:
         The extracted message.
@@ -325,44 +344,54 @@ def parse_message(element: ET.Element, bare_jid: str) -> Message:
         ValueError: in case there is malformed data not caught be the XML schema validation.
         XMLSchemaValidationError: in case the element does not conform to the XML schema given in the
             specification.
+
+    Warning:
+        This version of the OMEMO specification matches key material to recipient purely by device id. The
+        library, however, matches by bare JID and device id. Since the XML doesn't include the bare JID, the
+        structures expected by the library can't be filled correctly. Instead, to match the behaviour of the
+        specification, the bare JID of all key material included in the message is assigned to
+        ``recipient_bare_jid``, i.e. our own bare JID, which achieves the desired effect of matching only on
+        the device id.
     """
 
     MESSAGE_SCHEMA.validate(element)
 
     payload_elt = element.find(f"{NS}payload")
+    header_elt = cast(ET.Element, element.find(f"{NS}header"))
+    iv_elt = header_elt.find(f"{NS}iv")
 
     keys: Set[Tuple[EncryptedKeyMaterial, Optional[KeyExchange]]] = set()
 
-    for keys_elt in element.iter(f"{NS}keys"):
-        recipient_bare_jid = cast(str, keys_elt.get("jid"))
+    for key_elt in header_elt.iter(f"{NS}key"):
+        recipient_device_id = int(cast(str, key_elt.get("rid")))
+        content = base64.b64decode(cast(str, key_elt.text))
 
-        for key_elt in keys_elt.iter(f"{NS}key"):
-            recipient_device_id = int(cast(str, key_elt.get("rid")))
-            content = base64.b64decode(cast(str, key_elt.text))
+        key_exchange: Optional[KeyExchangeImpl] = None
+        authenticated_message: bytes
+        if bool(key_elt.get("prekey", False)):
+            key_exchange, authenticated_message = KeyExchangeImpl.parse(content)
+        else:
+            authenticated_message = content
 
-            key_exchange: Optional[KeyExchangeImpl] = None
-            authenticated_message: bytes
-            if bool(key_elt.get("kex", False)):
-                key_exchange, authenticated_message = KeyExchangeImpl.parse(content)
-            else:
-                authenticated_message = content
+        encrypted_key_material = EncryptedKeyMaterialImpl.parse(
+            authenticated_message,
+            recipient_bare_jid,
+            recipient_device_id
+        )
 
-            encrypted_key_material = EncryptedKeyMaterialImpl.parse(
-                authenticated_message,
-                recipient_bare_jid,
-                recipient_device_id
-            )
-
-            keys.add((encrypted_key_material, key_exchange))
+        keys.add((encrypted_key_material, key_exchange))
 
     return Message(
         NAMESPACE,
-        bare_jid,
-        int(cast(str, cast(ET.Element, element.find(f"{NS}header")).get("sid"))),
+        sender_bare_jid,
+        int(cast(str, header_elt.get("sid"))),
         (
             ContentImpl.make_empty()
-            if payload_elt is None
-            else ContentImpl(base64.b64decode(cast(str, payload_elt.text)))
+            if payload_elt is None or iv_elt is None
+            else ContentImpl(
+                base64.b64decode(cast(str, payload_elt.text)),
+                base64.b64decode(cast(str, iv_elt.text))
+            )
         ),
         frozenset(keys)
     )
