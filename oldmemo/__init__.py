19c45850993fef7e6ce7b3e892c2f6c593f334c6
Tim Henkes <me@syndace.dev>
2022-08-19 18:12:45 +0200

    Don't reexport migrations

--- oldmemo/__init__.py
+++ oldmemo/__init__.py
@@ -1,13 +1,3 @@
 from .version import __version__ as __version__
 
-from .migrations import (
-    BoundOTPK as BoundOTPK,
-    LegacyStorage as LegacyStorage,
-    OwnData as OwnData,
-    Session as Session,
-    State as State,
-    StateSuper as StateSuper,
-    Trust as Trust,
-    migrate as migrate
-)
 from .oldmemo import Oldmemo as Oldmemo
