ef13feb01d35eed47c66bbb14c0ee31c219843f1
Tim Henkes <me@syndace.dev>
2022-08-19 13:11:56 +0200

    Added migrations from legacy storage format

--- oldmemo/__init__.py
+++ oldmemo/__init__.py
@@ -1,3 +1,13 @@
 from .version import __version__ as __version__
 
+from .migrations import (
+    BoundOTPK as BoundOTPK,
+    LegacyStorage as LegacyStorage,
+    OwnData as OwnData,
+    Session as Session,
+    State as State,
+    StateSuper as StateSuper,
+    Trust as Trust,
+    migrate as migrate
+)
 from .oldmemo import Oldmemo as Oldmemo
